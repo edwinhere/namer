48f88091c9d895d7b3bb44d3aa31e50170ee3053
Edwin Jose Palathinkal <edwin@here.blue>
2026-05-09 15:24:12 +0800

    Remove pip-tmp from git and add to .gitignore

--- pip-tmp/jiti/agents-agent-scope.515c0bc4.mjs
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.resolveExecutionAgentScope = resolveExecutionAgentScope;
-
-function resolveExecutionAgentScope(scope) {
-  if (scope === "user" || scope === "project" || scope === "both") return scope;
-  return "both";
-} /* v9-28865fb4666b353e */
